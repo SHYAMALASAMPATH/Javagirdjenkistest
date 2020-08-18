6e9940393febf35e244299565153f50912cd94ef
SAMPATH <shyamalasampath484@gmail.com>
2020-08-18 15:58:03 +0530

    thrid commit

--- FirstTest.java
+++ FirstTest.java
@@ -6,10 +6,21 @@ public class FirstTest
 	{
 	System.out.println("this is my first java test from cmd");
       amethod();
+      bmethod();
+      cmethod();
     }
 	public static void amethod()
 	{
 		System.out.println("a method ");
 	}
+	public static void bmethod()
+	{
+		System.out.println("b method ");
+	}
+	public static void cmethod()
+	{
+		System.out.println("c method ");
+	}
+	
 }
 
