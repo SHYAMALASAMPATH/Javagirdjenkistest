ad984f76ead0dacb41cabca39fb6609445ba1629
SAMPATH <shyamalasampath484@gmail.com>
2020-08-18 15:29:37 +0530

    second commit update a method

--- FirstTest.java
+++ FirstTest.java
@@ -5,5 +5,11 @@ public class FirstTest
 	public static void main(String[] args)
 	{
 	System.out.println("this is my first java test from cmd");
+      amethod();
     }
+	public static void amethod()
+	{
+		System.out.println("a method ");
+	}
 }
+
